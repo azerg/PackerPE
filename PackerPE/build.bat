319c4331a787934693e078338f9e0c8f18324e03
azerg <pub@azerg.com>
2015-03-25 23:53:53 +0200

    + boost include\link

--- PackerPE/build.bat
+++ PackerPE/build.bat
@@ -1,1 +1,2 @@
-cmake . -B"build" -G"Visual Studio 12" -Wno-dev -DBOOST_FOLDER:STRING="F:\Work_Installs\boost_1_56_0"
+REM rd /s /q build
+cmake . -B"build" -G"Visual Studio 12" -Wno-dev -DBOOST_ROOT:STRING="F:\Work_Installs\boost_1_56_0"
