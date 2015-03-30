a7b23d79dd2e27ec79762890b2903eea47b61771
azerg <pub@azerg.com>
2015-03-30 20:16:51 +0300

    mf

--- PackerPE/spack/spack.cpp
+++ PackerPE/spack/spack.cpp
@@ -2,9 +2,11 @@
 #include "spack.h"
 #include "PeLib.h"
 
+#include <cstdint>
+
 typedef std::shared_ptr<PeLib::PeFile> PeFilePtr;
 
-void rebuildMZHeader(PeFilePtr& peFile, std::string& outFileName)
+uint64_t rebuildMZHeader(PeFilePtr& peFile, std::string& outFileName)
 {
   std::vector<PeLib::byte> mzHeadBuffer;
   peFile->mzHeader().rebuild(mzHeadBuffer);
@@ -12,14 +14,40 @@ void rebuildMZHeader(PeFilePtr& peFile, std::string& outFileName)
   PeLib::MzHeader mzOut;
   mzOut.read(mzHeadBuffer.data(), mzHeadBuffer.size(), 0);
   mzOut.write(outFileName, 0);
+
+  return mzOut.size();
 }
 
 template<int bits>
-void rebuildPEHeader(PeLib::PeFile& peFile, std::string& outFileName)
+uint64_t rebuildPEHeader(PeLib::PeFile& peFile, std::string& outFileName, uint64_t offset)
 {
-  const PeLib::PeHeaderT<bits>& peh = static_cast<PeLib::PeFileT<bits>&>(pef).peHeader();
-  ..
+  const PeLib::PeHeaderT<bits>& peh = static_cast<PeLib::PeFileT<bits>&>(peFile).peHeader();
+
+  std::vector<PeLib::byte> peHeaderBuff;
+  peh.rebuild(peHeaderBuff);
+
+  PeLib::PeHeaderT<bits> pehOut;
+  pehOut.read(peHeaderBuff.data(), peHeaderBuff.size(), offset);
+
+  return offset + pehOut.size();
 }
+
+//------------------------------------------------------------------------
+
+class DumpPeHeaderVisitor: public PeLib::PeFileVisitor
+{
+public:
+  DumpPeHeaderVisitor(std::string outFileName, uint64_t offset):
+    outFileName_(outFileName)
+  {}
+  virtual void callback(PeLib::PeFile32& file) { rebuildPEHeader<32>(file, outFileName_, offset_); }
+  virtual void callback(PeLib::PeFile64& file) { rebuildPEHeader<64>(file, outFileName_, offset_); }
+private:
+  std::string outFileName_;
+  uint64_t offset_;
+};
+
+//------------------------------------------------------------------------
 
 void PackExecutable(std::string srcFileName, std::string outFileName)
 {
@@ -31,25 +59,9 @@ void PackExecutable(std::string srcFileName, std::string outFileName)
   }
 
   pef->readMzHeader();
-  rebuildMZHeader(pef, outFileName);
+  auto offset = rebuildMZHeader(pef, outFileName);
 
   pef->readPeHeader();
-  DumpPeHeaderVisitor peVisitor(outFileName);
+  DumpPeHeaderVisitor peVisitor(outFileName, offset);
   pef->visit(peVisitor);
 }
-
-//------------------------------------------------------------------------
-
-class DumpPeHeaderVisitor: public PeLib::PeFileVisitor
-{
-public:
-  DumpPeHeaderVisitor(std::string outFileName) :
-    outFileName_(outFileName)
-  {}
-  virtual void callback(PeLib::PeFile32& file) { rebuildPEHeader<32>(file, outFileName_); }
-  virtual void callback(PeLib::PeFile64& file) { rebuildPEHeader<64>(file, outFileName_); }
-private:
-  std::string outFileName_;
-};
-
-//------------------------------------------------------------------------
---
+++ PackerPE/spack/tiny_logger.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+
+template<typename T>
+std::string toString(T x, char f = '0')
+{
+  std::stringstream ss;
+  ss << std::setw(sizeof(T) * 2) << std::setfill(f) << std::hex << std::uppercase << x;
+  return ss.str();
+}
+
+template<>
+std::string toString<PeLib::byte>(PeLib::byte x, char f)
+{
+  std::stringstream ss;
+  ss << std::setw(2) << std::setfill(f) << std::hex << std::uppercase << (int)x;
+  return ss.str();
+}
+
+std::string formatOutput(const std::string& text, const std::string& val, const std::string& pad = "", unsigned int maxsize = 70)
+{
+  std::stringstream ss;
+
+  ss << pad << text << std::setw(maxsize - text.length() - val.length() - pad.length()) << std::setfill(' ') << "";
+  ss << val;
+  return ss.str();
+}
+
+std::string centerOutput(const std::string& text, unsigned int maxsize = 70)
+{
+  std::stringstream ss;
+
+  unsigned int left = (maxsize - text.length()) / 2;
+  ss << std::setw(left) << std::setfill(' ') << "";
+  ss << text;
+
+  return ss.str();
+}
+
+void dump(const std::string& d)
+{
+  std::cout << d << std::endl;
+}
