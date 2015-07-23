f1b717015361672e68fe9d7a1dd295fdcc14bf52
azerg <pub@azerg.com>
2015-07-23 09:28:16 +0300

    moving to vs2015 :clap:

--- PackerPE/build.bat
+++ PackerPE/build.bat
@@ -1,2 +1,2 @@
 REM rd /s /q build
-cmake . -B"build" -G"Visual Studio 12" -Wno-dev -DBOOST_ROOT:STRING="F:\Work_Installs\boost_1_56_0"
+cmake . -B"build" -G"Visual Studio 14" -Wno-dev -DBOOST_ROOT:STRING="F:\Work_Installs\boost_1_58_0"
