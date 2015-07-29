146e969fd81b5fc0a4dcb9639505086722906beb
azerg <pub@azerg.com>
2015-07-29 17:44:29 +0300

    m.f.

--- PackerPE/common/inc/file_utils.h
+++ PackerPE/common/inc/file_utils.h
@@ -8,6 +8,6 @@ namespace file_utils
 {
   std::vector<uint8_t> readFile(const char* filename);
   std::vector<uint8_t> readFile(const std::string& filename);
-  void writeFile(const char* filename, uint32_t offset, const char* pData, uint32_t cbData);
+  void writeFile(const char* filename, std::streamoff offset, const char* pData, uint32_t cbData);
   void appendToFile(const char* filename, const char* pData, uint32_t cbData);
 } // namespace file_utils
