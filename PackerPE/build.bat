492dfbc73021989339aa333f3df71b817cfffb40
azerg <pub@azerg.com>
2018-02-23 17:08:07 +0200

    fixed visual studio default build

--- PackerPE/build.bat
+++ PackerPE/build.bat
@@ -1,2 +1,2 @@
 REM rd /s /q build
-cmake . -B"build" -G"Visual Studio 14" -Wno-dev --debug-output -DBOOST_ROOT:STRING="F:\Work_Installs\boost_1_58_0"
+cmake . -B"build" -G"Visual Studio 14" -Wno-dev --debug-output -DBOOST_ROOT:STRING="E:\common_libs\boost_1_60_0"
--- PackerPE/stub2rsrc/stub2rsrc.cpp
+++ PackerPE/stub2rsrc/stub2rsrc.cpp
@@ -23,26 +23,30 @@ int _tmain(int argc, _TCHAR* argv[])
     return -1;
   }
 
-  // we dont want to handle exceptions here coz we want to get notice if
-  // smth goes wrong
-
-  // here we will open target exe file ( we require read-only access )
-  PE::MemPEFile MemPE( argv[1], PE::MEMFILE_ACCESS::MA_READ );
-
-  if ( !MemPE.IsValidPEFile() )
+  try
   {
-    throw std::runtime_error( "not a valid PE file passed" );
+    // here we will open target exe file ( we require read-only access )
+    PE::MemPEFile MemPE(argv[1], PE::MEMFILE_ACCESS::MA_READ);
+
+    if (!MemPE.IsValidPEFile())
+    {
+      throw std::runtime_error("not a valid PE file passed");
+    }
+
+    PIMAGE_SECTION_HEADER pSecHead = MemPE.GetSectionHead();
+
+    std::ofstream fDestBin(argv[2], std::ios::out | std::ios::binary);
+    fDestBin.write(
+      reinterpret_cast<char*>(MemPE.GetMemPtr() + pSecHead->PointerToRawData),
+      pSecHead->SizeOfRawData);
+    fDestBin.close();
+
+    std::cout << "Stub created successfully in: ";
+    std::wcout << argv[2];
   }
-
-  PIMAGE_SECTION_HEADER pSecHead = MemPE.GetSectionHead();
-  
-  std::ofstream fDestBin( argv[2], std::ios::out | std::ios::binary );
-  fDestBin.write(
-    reinterpret_cast<char*>( MemPE.GetMemPtr() + pSecHead->PointerToRawData ),
-    pSecHead->SizeOfRawData );
-  fDestBin.close();
-
-  std::cout << "Stub created successfully in: ";
-  std::wcout << argv[2];
+  catch (const std::exception& ex)
+  {
+    std::cout << ex.what() << std::endl;
+  }
 }
 
